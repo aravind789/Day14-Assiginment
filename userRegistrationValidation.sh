2dc4287e5aea393ab6747f1a6d3efcca3b387ce3
aravind789 <aravindkris89@gmail.com>
2020-07-07 14:07:56 +0530

    Email Validation

--- userRegistrationValidation.sh
+++ userRegistrationValidation.sh
@@ -1,19 +1,28 @@
 #!/bin/bash -x
-shopt --glob
-read -p "Enter your first name :" firstName
-read -p "Enter your last name :" lastName
+#read -p "Enter your first name :" firstName
+#read -p "Enter your last name :" lastName
+read -p "Enter your Email :" email
 
-firstNamePattern="^[A-Z][a-z]{3,}$"
-lastNamePattern="^[A-Z][a-z]{3,}$"
-if [[ $firstName =~ $firstNamePattern ]] 
-then	echo "First name is valid"
-else
-	echo "First name is not valid, Type the first Letter in caps"
-fi
+#firstNamePattern="^[A-Z][a-z]{3,}$"
+#lastNamePattern="^[A-Z][a-z]{3,}$"
+emailPattern="^[a-zA-Z][0-9a-zA-Z\_\.\&\+\#]+[0-9a-zA-Z]+@[a-z]*\.[a-z]{2,3}\.[a-z]{2}$"
+#if [[ $firstName =~ $firstNamePattern ]]
+#then
+	#echo "First name is valid"
+#else
+	#echo "First name is not valid, Type the first Letter in caps"
+#fi
 
-if [[ $lastName =~ $lastNamePattern ]]
+#if [[ $lastName =~ $lastNamePattern ]]
+#then
+        #echo "Last name is valid"
+#else
+        #echo "Last name is not valid, Type the first Letter in caps"
+#fi
+
+if [[ $email =~ $emailPattern ]]
 then
-        echo "Last name is valid"
+        echo "Email is valid"
 else
-        echo "Last name is not valid, Type the first Letter in caps"
+        echo "Type a valid email address"
 fi
