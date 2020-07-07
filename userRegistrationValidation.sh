a296a28a88d3f51888c560de5dc552bc129af97e
aravind789 <aravindkris89@gmail.com>
2020-07-07 12:39:54 +0530

    Last name pattern added

--- userRegistrationValidation.sh
+++ userRegistrationValidation.sh
@@ -1,9 +1,19 @@
 #!/bin/bash -x
+shopt --glob
 read -p "Enter your first name :" firstName
+read -p "Enter your last name :" lastName
+
 firstNamePattern="^[A-Z][a-z]{3,}$"
-if [[ $firstName =~ $firstNamePattern ]]
-then
-	echo "First name is valid"
+lastNamePattern="^[A-Z][a-z]{3,}$"
+if [[ $firstName =~ $firstNamePattern ]] 
+then	echo "First name is valid"
 else
 	echo "First name is not valid, Type the first Letter in caps"
+fi
+
+if [[ $lastName =~ $lastNamePattern ]]
+then
+        echo "Last name is valid"
+else
+        echo "Last name is not valid, Type the first Letter in caps"
 fi
