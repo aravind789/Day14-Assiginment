b5cc5390d115585efe598e3ffc70819a668a16f8
aravind789 <aravindkris89@gmail.com>
2020-07-07 21:04:30 +0530

    Password validation

--- userRegistrationValidation.sh
+++ userRegistrationValidation.sh
@@ -3,12 +3,13 @@ read -p "Enter your first name :" firstName
 read -p "Enter your last name :" lastName
 read -p "Enter your Email :" email
 read -p "Enter your Phone number :" phoneNumber
-
+read -p "Enter your Password :" password
 
 firstNamePattern="^[A-Z][a-z]{3,}$"
 lastNamePattern="^[A-Z][a-z]{3,}$"
 emailPattern="^[a-zA-Z][0-9a-zA-Z\_\.\&\+\#]+[0-9a-zA-Z]+@[a-z]*\.[a-z]{2,3}\.[a-z]{2}$"
 phoneNumberPattern="^[0-9]{2}[]?[0-9]$"
+passwordPattern="^[a-zA-Z0-9\!\^\@\#$\&\]{8,}"
 if [[ $firstName =~ $firstNamePattern ]]
 then
 	echo "First name is valid"
@@ -35,4 +36,11 @@ then
         echo "Phone number is valid"
 else
         echo "Phone number is not valid"
+fi
+
+if [[ $password =~ $passwordPattern ]]
+then
+        echo "Password is valid"
+else
+        echo "Password is not valid"
 fi
