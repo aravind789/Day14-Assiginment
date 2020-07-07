d9dedd28034f1171ab7eae54cfc64d46ce2b8952
aravind789 <aravindkris89@gmail.com>
2020-07-07 12:04:25 +0530

    first name

--- userRegistrationValidation.sh
+++ userRegistrationValidation.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -x
-
-Welcome
+read -p "Enter your first name :" firstName
+firstNamePattern="^[A-Z][a-z]{3,}$"
+if [[ $firstName =~ $firstNamePattern ]]
+then
+	echo "First name is valid"
+else
+	echo "First name is not valid, Type the first Letter in caps"
+fi
